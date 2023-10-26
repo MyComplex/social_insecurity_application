cd3e3f8c640a15857866a55d9bd8f0f7c4988d3f
Jesus Santos <jo_nez@hotmail.com>
2023-10-26 00:22:02 -0700

    add and delete for virtuals

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,11 +1,18 @@
 /* IMPORT MODULES */
-const Thought = require('../models/Thought');
+const { Thought, User } = require('../models');
 
 /* CREATE CONTROLLER FUNCTIONS AND EXPORT */
 module.exports = {
     async createThought(req, res) {
         try {
-            const thought = await Thought.create(req.body);
+            const thought = await Thought.create(req.body)
+                .then(({ _id }) => {
+                    return User.findOneAndUpdate(
+                        { username: req.body.thoughtAuthor },
+                        { $push: { thoughts: _id } },
+                        { new: true, runValidators: true }
+                    );
+                });
             return res.status(200).json(thought);
         } catch (err) {
             return res.status(400).json(err);
@@ -52,4 +59,26 @@ module.exports = {
             return res.status(400).json(err);
         }
     },
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, { $addToSet: { reactions: req.body } }, { new: true, runValidators: true });
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            return res.status(200).json(thought);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    },
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { new: true, runValidators: true });
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            return res.status(200).json(thought);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -52,4 +52,26 @@ module.exports = {
             return res.status(400).json(err);
         }
     },
+    async addFriend(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.userId, { $addToSet: { friends: req.params.friendId } }, { new: true, runValidators: true });
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            return res.status(200).json(user);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    },
+    async deleteFriend(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.userId, { $pull: { friends: req.params.friendId } }, { new: true, runValidators: true });
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            return res.status(200).json(user);
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    }
 };
