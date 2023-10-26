94017ac52b71291b8875a45ca64df279102fc836
Jesus Santos <jo_nez@hotmail.com>
2023-10-26 00:23:54 -0700

    add & delete routes for virts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,12 @@
 /* IMPORT MODULES */
 const router = require('express').Router();
-const { createThought, getThought, getAllThoughts, updateThought, deleteThought } = require('../../controllers/thoughtController');
+const { createThought, getThought, getAllThoughts, updateThought, deleteThought, addReaction, deleteReaction } = require('../../controllers/thoughtController');
 
 /* DECLARE ROUTES */
 router.route('/').get(getAllThoughts).post(createThought);
 router.route('/:thoughtId').get(getThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 /* EXPORT ROUTER */
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,11 @@
 /* IMPORT MODULES */
 const router = require('express').Router();
-const { createUser, getUser, getAllUsers, updateUser, deleteUser } = require('../../controllers/userController');
+const { createUser, getUser, getAllUsers, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/userController');
 
 /* DECLARE ROUTES */
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 /* EXPORT ROUTER */
 module.exports = router;
